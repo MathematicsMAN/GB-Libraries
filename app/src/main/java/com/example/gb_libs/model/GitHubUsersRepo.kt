9609896d6ebb883a062edc97144e62673b0cb552
MathematicsMAN <damir_a_z@mail.ru>
2021-09-23 22:59:06 +0300

    HomeWork lesson 3

--- app/src/main/java/com/example/gb_libs/model/GitHubUsersRepo.kt
+++ app/src/main/java/com/example/gb_libs/model/GitHubUsersRepo.kt
@@ -1,5 +1,8 @@
 package com.example.gb_libs.model
 
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.kotlin.toObservable
+
 class GitHubUsersRepo {
     private val users = listOf(
         GitHubUser("user1"),
@@ -8,5 +11,5 @@ class GitHubUsersRepo {
         GitHubUser("user4"),
         GitHubUser("user5")
     )
-    fun getUsers() = users
+    fun getUsers() = users.toObservable()
 }
--- app/src/main/java/com/example/gb_libs/presentation/UsersPresenter.kt
+++ app/src/main/java/com/example/gb_libs/presentation/UsersPresenter.kt
@@ -1,11 +1,14 @@
 package com.example.gb_libs.presentation
 
+import android.util.Log
 import com.example.gb_libs.model.GitHubUser
 import com.example.gb_libs.model.GitHubUsersRepo
 import com.example.gb_libs.screens.AndroidScreens
 import com.example.gb_libs.view.UserItemView
 import com.example.gb_libs.view.ui.UserFragment
 import com.example.gb_libs.view.ui.UsersView
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
 import moxy.MvpPresenter
 import ru.terrakok.cicerone.Router
 
@@ -44,8 +47,33 @@ class UsersPresenter(
         }
     }
 
+    private val TAG = "UsersObserve"
+    private val users: MutableList<GitHubUser> = mutableListOf()
+
     fun loadData() {
-        val users = usersRepo.getUsers()
+        val userObserver = object : Observer<GitHubUser> {
+            override fun onSubscribe(d: Disposable) {
+                Log.d(TAG, "onSubscribe")
+            }
+
+            override fun onNext(user: GitHubUser) {
+                Log.d(TAG, "onNext: ${user.login}")
+                users.add(user)
+            }
+
+            override fun onError(e: Throwable) {
+                Log.d(TAG, "onError")
+            }
+
+            override fun onComplete() {
+                Log.d(TAG, "onComplete")
+            }
+
+        }
+
+        usersRepo.getUsers()
+            .subscribe(userObserver)
+
         usersListPresenter.users.addAll(users)
         viewState.updateList()
     }
